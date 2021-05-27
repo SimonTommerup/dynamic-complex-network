b22600aa7d37286c9e476679f070251fc98165ae
SimonTommerup <simontommerup@outlook.dk>
2021-05-27 14:52:44 +0200

    working simulation for several nodes

--- simulate_nhpp.py
+++ simulate_nhpp.py
@@ -7,12 +7,13 @@ from scipy.spatial.distance import pdist, squareform
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
+np.random.seed(1)
 
 ns = NodeSpace()
 n_clusts = 3
-n_points = [1, 1, 1]
+n_points = [7, 7, 7]
 centers = [[2,-1], [-3,0], [3,3]]
-radius = [0,0,0]
+radius = [2.1,2.5,1.9]
 v = [[0,1], [1,0], [0,-1]]
 a =  [[0,0], [0,0], [0,0]]
 
@@ -21,6 +22,7 @@ v0, a0 = ns.init_dynamics(n_clusts, n_points, v, a)
 ns.init_conditions(z0, v0, a0)
 
 print(ns.z0)
+
 
 # %%
 # Objective: Simulate n(n-1)/2 nhpp at a time
@@ -48,17 +50,17 @@ def root_matrix(ns):
         d = p1*(z_ux - z_vx) + p3*(z_uy - z_vy)
 
         r = np.roots([a,b,c,d])
-        r = r[np.isreal(r)]
+        r = r[np.isreal(r)] # real solutions
+        r = r[r >= 0]       # positive time axis
         r = np.sort(r)
         
         rmat[u, v] = r
     return rmat
 
 rmat = root_matrix(ns)
-print(rmat)
+
 # %%
-## Monotonicity
-# 
+
 def monotonicity_mat(ns, root_matrix):
     beta = ns.beta
     n_points = len(ns.z0)
@@ -68,6 +70,13 @@ def monotonicity_mat(ns, root_matrix):
     for u, v in zip(ind[0], ind[1]):
         i = 0
         roots = root_matrix[u,v]
+
+        # If no real, non-negative roots, then the intensity rate is
+        # either constant or monotonously increasing on positive time axis
+        if len(roots) == 0:
+            mmat[u,v] = np.array([])
+            continue
+
         time_points = [roots[0] - 0.5] # t to get sign of dLambda(t)/dt before first root
         while i < len(roots)-1:
             t = roots[i] + (roots[i+1] - roots[i]) / 2 # t's to get sign of dLambda(t)/dt between roots
@@ -90,7 +99,139 @@ def monotonicity_mat(ns, root_matrix):
 
 mmat = monotonicity_mat(ns, rmat)
 
+# %%
+
+# map time points to monotonicity
+def time_to_monotonicity(time, roots, monotonicity):
+    rprev = -np.inf
+    cur_r, cur_m = 0, 0
+    last_m = False if len(roots) > 1 else True
+    ttm = []
+
+    if len(monotonicity)==0:
+        return ttm
+        
+    for t in time:
+        if (rprev < t) & (t < roots[cur_r]):
+            ttm.append(monotonicity[cur_m])
+        elif last_m:
+            ttm.append(monotonicity[cur_m+1])
+        elif (roots[cur_r] < t) & (t < roots[cur_r+1]):
+            rprev = roots[cur_r]
+            cur_r += 1
+            cur_m += 1
+            if cur_r == len(roots) - 1:
+                last_m = True
+            ttm.append(monotonicity[cur_m])
+    return ttm
+
+# find intensity upperbounds for all time intervals
+def upperbounds(ns, u, v, time, roots, time_to_monotonicity):
+    beta = ns.beta
+    lambda_arr = []
+    mon = time_to_monotonicity
+    rcount=0
+    idx = 0
+
+    if len(mon)==0:
+        while idx < len(time)-1:
+            cur_lambda = ns.lambda_fun(time[idx], u, v)
+            next_lambda = ns.lambda_fun(time[idx+1], u, v)
+            lambda_arr.append(np.maximum(cur_lambda, next_lambda))
+            idx += 1
+    else:
+        while idx < len(time)-1:
+            if mon[idx] != mon[idx + 1]:
+                lambda_root = ns.lambda_fun(roots[rcount], u, v)
+
+                if mon[idx] == "dec":
+                    lambda_next_t = ns.lambda_fun(time[idx+1], u, v)
+                
+                max_lambda = np.maximum(lambda_root, lambda_next_t)
+                lambda_arr.append(max_lambda)
+                rcount += 1
+            
+            elif mon[idx] == "inc":
+                lambda_next_t = ns.lambda_fun(time[idx+1], u, v)
+                lambda_arr.append(lambda_next_t)
+
+            elif mon[idx] == "dec":
+                lambda_cur_t = ns.lambda_fun(time[idx], u, v)
+                lambda_arr.append(lambda_cur_t)
+
+            idx += 1
+
+    lambda_arr = np.array(lambda_arr)
+    return lambda_arr
+
+def nhpp(ns, u, v, time, upperbounds):
+    beta = ns.beta
+    interval = 0
+    t = 0
+    event_times = [] 
+    exceed_interval = False
+    stop = False 
+
+    while not stop:
+        if not exceed_interval:
+            u1 = np.random.uniform(0,1)
+            x = -1 / upperbounds[interval] * np.log(u1)
+
+        var = t+x
+        if (t + x) < time[interval + 1]:
+            exceed_interval = False
+
+            t += x
+            u2 = np.random.uniform(0,1)
+
+            lambda_t = ns.lambda_fun(t, u, v)
+            prob = lambda_t / upperbounds[interval]
+
+            assert lambda_t <= upperbounds[interval], "Lambda value exceeds upperbound"
+            assert prob <= 1, "Probability out of range >1"
+            assert prob >= 0, "Probability out of range <0"
+
+            if u2 < prob: # accept event time t if u2 < lambda(t) / lambda upperbound
+                event_times.append(t)
+
+        elif (t + x) >=  time[interval + 1]:
+            exceed_interval = True
+            if interval == len(upperbounds) - 1:
+                stop = True
+            else:
+                x = (x - time[interval + 1] + t) * upperbounds[interval] / upperbounds[interval+1]
+                t = time[interval + 1]
+                interval += 1
+
+    return np.array(event_times)
 
 # %%
-print(mmat)
-# %%
+
+def nhpp_mat(ns, time, root_matrix, monotonicity_matrix):
+    beta = ns.beta
+    n_points = len(ns.z0)
+    ind = np.triu_indices(n_points, k=1)
+    nhpp_mat = np.zeros(shape=(n_points, n_points), dtype=object)
+
+    for u, v in zip(ind[0], ind[1]):
+        r = root_matrix[u,v]
+        m = monotonicity_matrix[u,v]
+
+        t2m = time_to_monotonicity(time, roots=r, monotonicity=m) # map time to monotonicity
+        ubl = upperbounds(ns, u, v, time, roots=r, time_to_monotonicity=t2m) # find upperbounds for all time intervals
+        nhpp_sim = nhpp(ns, u, v, time=time, upperbounds=ubl) # simulate nhpp
+
+        nhpp_mat[u,v] = nhpp_sim
+    
+    return nhpp_mat
+
+
+
+t = np.linspace(0, 10, 25)
+nhppmat = nhpp_mat(ns=ns, time=t, root_matrix=rmat, monotonicity_matrix=mmat)
+
+e = nhppmat[0,20]
+plt.hist(e)
+plt.show()
+
+print(rmat[0,20])
